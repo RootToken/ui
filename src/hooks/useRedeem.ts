a1285b680bef7470f5f1ea85395b111486771fdc
0xkokonut <0xkokonut@gmail.com>
2022-11-16 17:43:26 -0800

    redeem

--- src/hooks/useRedeem.ts
+++ src/hooks/useRedeem.ts
@@ -5,7 +5,6 @@ import {
   Clipboard,
   Token,
 } from "@beanstalk/sdk";
-import { DepositTransferStruct } from "@beanstalk/sdk/dist/types/constants/generated/Beanstalk/Root";
 import { SignedPermit } from "@beanstalk/sdk/dist/types/lib/permit";
 import { ethers } from "ethers";
 import TransactionToast from "../components/Common/TransactionToast";
@@ -37,11 +36,13 @@ export default function useRedeem() {
       });
 
       const txn = await beanstalkSdk.contracts.root.redeem(
-        [{
-          token: beanstalkSdk.tokens.BEAN.address,
-          seasons,
-          amounts
-        }],
+        [
+          {
+            token: beanstalkSdk.tokens.BEAN.address,
+            seasons,
+            amounts,
+          },
+        ],
         FarmFromMode.EXTERNAL,
         maxRootsIn.toBlockchain()
       );
@@ -73,57 +74,97 @@ export default function useRedeem() {
         amounts.push(deposit.amount.toBlockchain());
       });
 
+      console.log("SDSDJSD", amount.toHuman())
+
+      seasons.forEach((v) => console.log(v.toString()))
+      amounts.forEach((v) => console.log(v.toString()))
+
       const token = beanstalkSdk.tokens.ROOT;
 
-      const farm = beanstalkSdk.farm.create("Redeem", "beanstalk");
+      const depotFarm = beanstalkSdk.farm.create<{ permit: any }>(
+        "DepotMint",
+        "depot"
+      );
+
+      // Give DEPOT permission to use `inputToken`.
+      depotFarm.add(new beanstalkSdk.farm.actions.PermitERC20(permit), {
+        onlyExecute: true,
+        // skip: (amountInStep) => inputToken.hasEnoughAllowance(account.address, spender, amountInStep)
+      });
+
+      depotFarm.add(
+        beanstalkSdk.farm.presets.loadPipeline(token, FarmFromMode.EXTERNAL),
+        { onlyExecute: true }
+      );
+
       const pipe = beanstalkSdk.farm.createAdvancedPipe();
 
-      farm.add(new beanstalkSdk.farm.actions.PermitERC20(permit));
-      farm.add(
-        beanstalkSdk.farm.presets.loadPipeline(
-          token,
-          FarmFromMode.EXTERNAL,
-        )
-      );
-      farm.add(
-        pipe.add([
-          async (amountInStep) => {
-            const amountOut = amountInStep; // FIXME
-            return pipe.wrap(
-              beanstalkSdk.contracts.root,
-              "redeem",
-              [
-                [
-                  {
-                    token: beanstalkSdk.tokens.BEAN.address,
-                    seasons,
-                    amounts,
-                  },
-                ],
-                FarmToMode.EXTERNAL, // send tokens to PIPELINE's external balance
-                maxRootsIn.toBlockchain(),
-              ],
-              amountOut // pass this to next element
-            );
-          },
-          (amountInStep) =>
-            pipe.wrap(
-              beanstalkSdk.contracts.beanstalk,
-              "transferDeposits",
-              [
-                /*  36 */ beanstalkSdk.contracts.pipeline.address,
-                /*  68 */ account!.address,
-                /* 100 */ beanstalkSdk.tokens.BEAN.address, // Will be overwritten by advancedData
-                /* 132 */ seasons, // use PIPELINE's external balance
-                /* 164 */ amounts, // TOOD: make this a parameter
-              ],
-              amountInStep
-            ),
-        ])
+      pipe.add(function redeem(amountInStep) {
+        return pipe.wrap(
+          beanstalkSdk.contracts.root,
+          "redeem",
+          [
+            [
+              {
+                token: beanstalkSdk.tokens.BEAN.address,
+                seasons,
+                amounts,
+              },
+            ],
+            FarmToMode.EXTERNAL, // send tokens to PIPELINE's external balance
+            maxRootsIn.toBlockchain(),
+          ],
+          amountInStep // pass this to next element
+        );
+      }, {});
+
+      pipe.add(function transfer(amountInStep) {
+        return pipe.wrap(
+          beanstalkSdk.contracts.beanstalk,
+          "transferDeposits",
+          [
+            /*  36 */ beanstalkSdk.contracts.pipeline.address,
+            /*  68 */ account!.address,
+            /* 100 */ beanstalkSdk.tokens.BEAN.address, // Will be overwritten by advancedData
+            /* 132 */ seasons, // use PIPELINE's external balance
+            /* 164 */ amounts, // TOOD: make this a parameter
+          ],
+          amountInStep
+        );
+      }, {});
+
+      pipe.add(
+        () => ({
+          target: beanstalkSdk.contracts.root.address,
+          callData: beanstalkSdk.contracts.root.interface.encodeFunctionData(
+            "balanceOf",
+            [beanstalkSdk.contracts.pipeline.address]
+          ),
+        }),
+        { tag: "rootLeftOver" }
       );
 
+      pipe.add(async function deposit(amountInStep, context) {
+        return pipe.wrap(
+          beanstalkSdk.contracts.root,
+          "transfer",
+          [
+            /* 0 */ account!.address,
+            /* 1 */ amountInStep, // PASTE HERE
+          ],
+          amountInStep,
+          // Find the step tagged `amountToDeposit`
+          // Copy from the first return return value (0)
+          // Paste into the second argument of `deposit` (1).
+          Clipboard.encodeSlot(context.step.findTag("rootLeftOver"), 0, 1)
+        );
+      });
+
+      depotFarm.add(pipe);
+
+
       // @TODO add a farm step here to withdraw
-      farm.add(
+      depotFarm.add(
         new beanstalkSdk.farm.actions.WithdrawDeposits(
           beanstalkSdk.tokens.BEAN.address,
           seasons,
@@ -131,9 +172,11 @@ export default function useRedeem() {
         )
       );
 
-      const txn = await farm.execute(amount, {
-        slippage: 0.5,
-      });
+      const txn = await depotFarm.execute(amount, {
+        slippage: 0.1,
+        permit: '',
+      })
+      
       txToast.confirming(txn);
       const receipt = await txn.wait();
       txToast.success(receipt);
